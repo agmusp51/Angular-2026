3ceb69aa4cd8f1ab6c8ebaf087c0f196ded1c016
Andres Guigou <guigouandres@gmail.com>
2026-06-05 14:51:52 -0300

    TP2

--- Tarea-2-app/src/app/app.ts
+++ Tarea-2-app/src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MIN_LENGTH, REQUIRED } from '@angular/forms/signals';
+import { REACTIVE_NODE } from '@angular/core/primitives/signals';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ReactiveFormsModule],
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -47,7 +50,35 @@ export class App {
 
   submitRegisterForm(){
       console.log("Se envio el formulario de registro", this.register_form.value
-         
       )
+  }
+
+  getRegisterFormError(field_name: string) :string{
+    const register_form_control =this.register_form.get(field_name)
+    console.log(register_form_control?.errors)
+    if(!register_form_control?.touched){
+      return''
+    }
+    
+    if (!register_form_control){
+      return ''
+    }
+    if (register_form_control.hasError(ERRORTIPO.REQUIRED.NAME)){
+      return 'Este campo es obligatorio'
+    }
+    if (register_form_control.hasError(ERRORTIPO.MIN_LENGTH.NAME)){
+      const cant = register_form_control.errors?.[ERRORTIPO.MIN_LENGTH.NAME].requiredLength
+      return 'Debe tener al menos ' + cant + 'caracteres'
+    }
+    return ''
+  }
+}
+
+const ERRORTIPO = {
+  MIN_LENGTH:{
+    NAME: 'minlength',
+  },
+   REQUIRED:{
+    NAME: 'required', 
   }
 }
